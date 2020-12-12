0e01ee1169df8c688097c98631fb999997d6b5c7
Jordan Newberry <jordan.newberry@gmail.com>
2020-12-12 13:50:59 -0600

    added nav from support view to comments view.

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -4,10 +4,18 @@ import { connect } from 'react-redux';
 
 //class
 class Support extends Component{
+
+    nextPage = () => {
+        console.log('In nextPage button.....')
+        this.props.history.push('/comments')
+    }
+
     render(){
         return(
             <div>
-                <h2>Hello from Support component </h2>
+                <h2>How well are you feeling supported?</h2>
+                <input type="number" max="6" label="supported?" />
+                <button onClick={this.nextPage}>NEXT</button>
             </div>
         ) //end return 
     } //end render
